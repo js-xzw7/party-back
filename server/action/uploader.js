9c323d7cf0ca0c568d536e7b6b900370b90e71c1
lintry <shenlin00@gmail.com>
2018-01-15 15:50:52 +0800

    升级库，调整上传模块，去除do-action

--- server/action/uploader.js
+++ server/action/uploader.js
@@ -4,49 +4,67 @@
 /* global global */
 /* global process */
 "use strict";
-function Uploader(routerPath) {
+
+/**
+ * 上传、下载文件
+ * @param routerPath
+ * @return {*}
+ * @constructor
+ */
+function Uploader (routerPath) {
     routerPath = routerPath || '';
 
     const _ = require('lodash'),
         path = require('path'),
-        po = global.po,
         fs = require('fs-extra'),
         Formidable = require('formidable'),
         gm = require('gm'),
         express = require('express'),
+        mime = require('mime'),
         router = express.Router(),
         UPLOAD_OPTION = global.config.upload,
+        moment = require('moment'),
         logger = global.loggers.upload,
         db = global.sequelize,
+        po = global.po,
         crypto_utils = require('kml-crypto-utils'),
         Result = require('kml-express-stage-lib').Result;
 
     const
         DEST_DIR = path.resolve(UPLOAD_OPTION.root, routerPath),
         IMAGE_URL = (UPLOAD_OPTION.base_url || '/') + routerPath + '/',
-        THUMB_WIDTH = 240;
+        THUMB_WIDTH = 240,
+        Default_Plat = 'saas';
+
+    //自定义mime类型
+    mime.define({'image/jpg': ['jpg']}, true); //修复微信上传的错误
+    mime.define({'image/jpeg': ['jpg', 'jpeg', 'jpe']}, true); //调整常用的jpg后缀
+    mime.define({'video/quicktime': ['mov', 'qt']}, true); //调整常用的quicktime视频后缀
 
     //定义上传文件的访问路径
     router.use(`/${routerPath}`, express.static(DEST_DIR));
 
-    const FileInfo = po.import(db, 'file_info');
+    // const [File] = po.import(db, ['file']);
 
+    //定义上传文件输出结构
+    const FILE_ATTRIBUTES = ['file_id', 'plat_id', 'name', 'type', 'content_type', 'name', 'size', 'file_path', 'orig_path', 'hash_value', 'url', 'thumbnail'];
+    const FILE_GET_ATTRIBUTES = ['file_id', 'plat_id', 'name', 'type', 'content_type', 'name', 'size', 'hash_value', 'url', 'thumbnail'];
     const upload_root = path.resolve(UPLOAD_OPTION.root);
+
     fs.ensureDirSync(upload_root);
     fs.ensureDirSync(DEST_DIR);
 
-    function fn_request(req, res, next) {
+    function upload (req, res, next) {
         /**
          * 创建缩略图
          * @param gm_object gm对象,可以在此前对图片做好预处理
          * @param file_orig 原始处理图片文件
          * @param uploaded_file 上传文件结构
          */
-        function create_thumb(gm_object, file_orig, uploaded_file) {
+        function create_thumb (gm_object, file_orig, uploaded_file) {
             let dest_thumb = uploaded_file.thumb_path, //缩略图
                 dest_file = uploaded_file.file_path, //优化后的图片
                 dest_orig = uploaded_file.orig_path; //原始图片
-
             gm_object.size(function (err, size) {
                 if (err) {
                     logger.error(err, uploaded_file);
@@ -61,117 +79,166 @@ function Uploader(routerPath) {
                         gm(dest_file).thumb(thumb_w, thumb_w, dest_thumb, 0, 'center', function (err) {
                             err ? logger.error(err, uploaded_file) : logger.debug('image saved to', uploaded_file);
                             //move file to dest_orig
-                            fs.rename(file_orig.path, dest_orig);
+                            fs.renameSync(file_orig.path, dest_orig);
                         });
                     }
                 })
             });
+        }
+
+        /**
+         * 非图片移动到目标文件夹
+         * @param file_orig
+         * @param uploaded_file
+         * @return {null}
+         */
+        const rename = function (file_orig, uploaded_file) {
+            let dest_orig = uploaded_file.file_path; //原始图片
+            fs.renameSync(file_orig.path, dest_orig);
         };
 
-        // parse a file upload
+        const upload_date = moment().format('YYYY/MM/DD');
+
+        //根据年月日同步创建文件夹
+        fs.ensureDirSync(path.resolve(DEST_DIR + '/' + upload_date));
+
+        //上传文件
         let form = new Formidable.IncomingForm();
         form.encoding = 'utf-8';
-        form.uploadDir = DEST_DIR;
+        form.uploadDir = path.resolve(DEST_DIR + '/' + upload_date);
         form.hash = 'md5';
+        form.multiples = true;
         form.keepExtensions = true;
-        form.maxFieldsSize = 10 * (1 << 20); //max file size is 10M
+        form.maxFieldsSize = 50 * (1 << 20); //max file size is 10M
         // 处理上传文件
         form.parse(req, function (err, fields, files) {
             let uploaded_files = [];
+            console.log('upload', fields, files)
             if (files) {
                 for (let key in files) {
                     if (files.hasOwnProperty(key)) {
-                        let file = files[key];
-                        if (file && file.size) {
-                            let file_ext = file.type.replace('image/', ''), orig_ext = file_ext;
-                            switch (file.type) {
-                                case 'image/gif':
-                                    file_ext = 'gif';
-                                    break;
-                                case 'image/jpeg':
-                                    file_ext = 'jpg';
-                                    break;
-                                case 'image/png':
-                                    file_ext = 'png';
-                                    break;
-                                default:
-                                    res.json(new Result(Result.ERROR, '上传文件格式错误'));
+                        let files_array = [].concat(files[key]);
+                        files_array.forEach(file => {
+                            if (file && file.size) {
+                                if (file.type === 'image/jpg') {
+                                    file.type = 'image/jpeg' //修复微信上传图片的错误
+                                }
+                                let file_ext = mime.getExtension(file.type);
+                                if (!file_ext) {
+                                    logger.error('上传文件格式错误', file.type);
+                                    res.json(Result.Error('上传文件格式错误', file.type));
                                     return null;
+                                }
+
+
+                                const plat_id = req.session.active_user && req.session.active_user.plat_id
+                                    ? req.session.active_user.plat_id : Default_Plat;
+
+                                let regexp = /\.([\w]{3,})$/,
+                                    salt = (~~(Math.random() * 1000000000)).toString(36),
+                                    file_id = crypto_utils.UUID(),
+                                    file_name = `${file_id}_${salt}.${file_ext}`,
+                                    orig_file_name = `${file_id}_${salt}_o.${file_ext}`,
+                                    dest_file = path.resolve(DEST_DIR + '/' + upload_date, file_name),
+                                    dest_orig = path.resolve(DEST_DIR + '/' + upload_date, orig_file_name),
+                                    dest_thumb = dest_file.replace(regexp, '_s.$1'),
+                                    uploaded_file = {
+                                        file_id: file_id,
+                                        plat_id: plat_id,
+                                        content_type: file.type,
+                                        name: file.name,
+                                        size: file.size,
+                                        file_path: dest_file,
+                                        thumb_path: dest_thumb,
+                                        orig_path: dest_orig,
+                                        hash_value: file.hash,
+                                        url: IMAGE_URL + upload_date + '/' + file_name,
+                                        params: fields,
+                                        key: key,
+                                        thumbnail: IMAGE_URL + upload_date + '/' + file_name.replace(regexp, '_s.$1')
+                                    };
+
+                                uploaded_files.push(uploaded_file);
+
+                                switch (file.type) {
+                                    case 'image/gif':
+                                        create_thumb(gm(file.path + '[0]'), file, uploaded_file);
+                                        break;
+                                    case /^image\/(jpg|jpeg|png)$/.test(file.type) && file.type:
+                                        create_thumb(gm(file.path).autoOrient(), file, uploaded_file);
+                                        break;
+                                    case /^application\/[\w-+.]+$/.test(file.type) && file.type:
+                                        rename(file, uploaded_file);
+                                        break;
+                                    case /^video\/[\w-+.]+$/.test(file.type) && file.type:
+                                        rename(file, uploaded_file);
+                                        break;
+                                    default:
+                                        fs.unlinkSync(file.path);
+                                        logger.error(`${file.name} is not jpg/png/gif but ${file.type}`);
+
+                                        res.json(Result.Ok('success', {files: uploaded_files}));
+                                        return null;
+                                }
                             }
-
-                            let regexp = /\.([\w]{3,})$/,
-                                salt = (~~(Math.random() * 1000000000)).toString(36),
-                                file_id = crypto_utils.UUID(),
-                                file_name = `${file_id}_${salt}.${file_ext}`,
-                                orig_file_name = `${file_id}_${salt}_o.${orig_ext}`,
-                                dest_file = path.resolve(DEST_DIR, file_name),
-                                dest_orig = path.resolve(DEST_DIR, orig_file_name),
-                                dest_thumb = dest_file.replace(regexp, '_s.$1'),
-                                uploaded_file = {
-                                    id: file_id,
-                                    content_type: file.type,
-                                    name: file.name,
-                                    size: file.size,
-                                    file_path: dest_file,
-                                    thumb_path: dest_thumb,
-                                    orig_path: dest_orig,
-                                    hash: file.hash,
-                                    url: IMAGE_URL + file_name,
-                                    params: fields,
-                                    key: key,
-                                    thumbnail: IMAGE_URL + file_name.replace(regexp, '_s.$1')
-                                };
-
-                            switch (file.type) {
-                                case 'image/gif':
-                                    create_thumb(gm(file.path + '[0]'), file, uploaded_file);
-                                    break;
-                                case 'image/jpeg':
-                                case 'image/png':
-                                    create_thumb(gm(file.path).autoOrient(), file, uploaded_file);
-                                    break;
-                                default:
-                                    fs.unlinkSync(file.path);
-                                    logger.error(`${file.name} is not jpg/png/gif but ${file.type}`);
-                                    break;
-                            }
-
-                            uploaded_files.push(uploaded_file);
-                        }
+                        })
                     }
                 }
             }
 
-            //处理结果
-            if (uploaded_files.length) {
-                FileInfo.bulkCreate(uploaded_files)
-                    .then(function (files) {
-                        logger.debug('file saved into db', files);
-                    })
-            }
-
             //输出结果
-            res.json(new Result(Result.OK, undefined, undefined, {files: uploaded_files}));
-            next && next();
+            // todo 存储文件信息
+            res.json(Result.Ok('success', {files: uploaded_files}));
             return null;
-        });
-    };
 
-    let upload_regexp = /(.*)\/upload(\.do)?$/;
-    router.post(upload_regexp, fn_request);
+        })
+
+    }
+
+
+    function getFileByHash (hash) {
+        //查询原有文件的hash值
+        let hash_where = {hash_value: hash};
+        // return File.findOne({
+        //     attributes: FILE_GET_ATTRIBUTES,
+        //     where: hash_where,
+        //     sort: [['create_date', 'desc']]
+        // })
+    }
+
+    function findByHash (req, res, next) {
+        let hash = req.query.hash;
+        //查询原有文件的hash值
+        return getFileByHash(hash)
+            .then(function (file_result) {
+                return res.json(Result.Ok('success', file_result));
+            })
+    }
+
+    let upload_regexp = /(.*)\/upload(\.do)?$/,
+        hash_regexp = /(.*)\/hash(\.do)?$/;
+    router.post(upload_regexp, upload);
+    router.get(hash_regexp, findByHash);
     router.get(upload_regexp, function (req, res, next) {
         res.writeHead(200, {'content-type': 'text/html'});
-        res.end('\
-  <article>\
-  <form action="" enctype="multipart/form-data" method="post">\
-    <input type="text" name="title" placeholder="图片简介" style="width: 100%;"><br>\
-    <input type="file" name="upload" multiple="multiple" style="width: 50%;">\
-    <input type="submit" value="上传" style="width: 30%;">\
-  </form>\
-  </article>'
+        res.end(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<article>
+  <form action="" enctype="multipart/form-data" method="post">
+    <input type="text" name="title" placeholder="图片简介" style="width: 100%;"><br>
+    <input type="file" name="upload" multiple="multiple" style="width: 50%;">
+    <input type="submit" value="上传" style="width: 30%;">
+  </form>
+</article>
+</body>
+</html>
+`
         );
-
-        next && next();
     });
 
     return router;
--- server/init/log4js-init.js
+++ server/init/log4js-init.js
@@ -1,91 +1,115 @@
 /**
- * Created by lintry on 16/4/15.
+ * Created by lintry on 17/7/18.
+ * log4js 2.x 初始化定义
  */
 "use strict";
-const loggers = function(){
-  const log4js = require('log4js'),
-    fs = require('fs-extra'),
-    path = require('path'),
-    config = require('./config'),
-    config_path = config.path,
-    project_name = config.system.project_name;
+const loggers = function () {
+    const semver = require('semver'),
+        _ = require('lodash'),
+        log4js = require('log4js'),
+        fs = require('fs-extra'),
+        path = require('path'),
+        config = require('./config'),
+        config_path = config.path,
+        project_name = config.system.project_name;
 
-  const log_path = config_path.LOGS_PATH;
-  fs.ensureDirSync(log_path);
+    const log_path = config_path.LOGS_PATH;
+    fs.ensureDirSync(log_path);
 
-  const appenders = [
-    { type: 'console' },
-    {
-      type: 'file',
-      filename: path.resolve(log_path, `${project_name}.log`),
-      maxLogSize: 20480000,
-      backups: 30,
-      category: 'project',
-      level: 'WARN'
-    },
-    {
-      type: 'file',
-      filename: path.resolve(log_path, 'system.log'),
-      maxLogSize: 20480000,
-      backups: 30,
-      category: 'system',
-      level: 'ALL'
-    },
-    {
-      type: 'file',
-      filename: path.resolve(log_path, 'upload.log'),
-      maxLogSize: 20480000,
-      backups: 30,
-      category: 'upload',
-      level: 'ERROR'
-    },
-    {
-      type: 'file',
-      filename: path.resolve(log_path, 'pay.log'),
-      maxLogSize: 20480000,
-      backups: 30,
-      category: 'pay',
-      level: 'ALL'
-    },
-    {
-      type: 'file',
-      filename: path.resolve(log_path, 'wx.log'),
-      maxLogSize: 20480000,
-      backups: 30,
-      category: 'wx',
-      level: 'ALL'
-    },
-    {
-      type: 'file',
-      filename: path.resolve(log_path, 'cron.log'),
-      maxLogSize: 20480000,
-      backups: 30,
-      category: 'cron',
-      level: 'ALL'
+    const pkg = require(path.resolve(process.cwd(), 'node_modules/log4js/package.json')),
+        log4js_2_x = semver.gt(pkg.version, '2.0.0');
+
+    const appenders = [
+        {type: 'console'},
+        {
+            type: 'file',
+            filename: path.resolve(log_path, `${project_name}.log`),
+            maxLogSize: 20480000,
+            backups: 30,
+            category: 'project',
+            level: 'WARN'
+        },
+        {
+            type: 'file',
+            filename: path.resolve(log_path, 'system.log'),
+            maxLogSize: 20480000,
+            backups: 30,
+            category: 'system',
+            level: 'ALL'
+        },
+        {
+            type: 'file',
+            filename: path.resolve(log_path, 'upload.log'),
+            maxLogSize: 20480000,
+            backups: 30,
+            category: 'upload',
+            level: 'ERROR'
+        },
+        {
+            type: 'file',
+            filename: path.resolve(log_path, 'pay.log'),
+            maxLogSize: 20480000,
+            backups: 30,
+            category: 'pay',
+            level: 'ALL'
+        },
+        {
+            type: 'file',
+            filename: path.resolve(log_path, 'wx.log'),
+            maxLogSize: 20480000,
+            backups: 30,
+            category: 'wx',
+            level: 'ALL'
+        },
+        {
+            type: 'file',
+            filename: path.resolve(log_path, 'cron.log'),
+            maxLogSize: 20480000,
+            backups: 30,
+            category: 'cron',
+            level: 'ALL'
+        }
+    ];
+
+    if (log4js_2_x) {
+        const _appenders = {},
+            _categories = {};
+
+        appenders.forEach(function (appender) {
+            let name = appender.category || 'default';
+            _appenders[name] = _.omit(appender, ['category', 'level']);
+            _categories[name] = { appenders: [name, 'default'], level: appender.level || 'ALL'};
+        });
+
+        log4js.configure({
+            appenders: _appenders,
+            categories: _categories,
+            replaceConsole: true,
+            pm2: true
+        });
+    } else {
+        log4js.configure({
+            appenders: appenders
+        });
     }
-  ];
 
-  log4js.configure({
-    appenders: appenders
-  });
+    const logger_export = {};
+    appenders.forEach(function (appender) {
+        let name = appender.category;
+        if (name) {
+            let logger = log4js.getLogger(name);
+            logger_export[name] = logger;
+            !log4js_2_x && logger.setLevel(appender.level || 'ALL');
+            logger.trace('TRACE is enabled now!');
+            logger.debug('DEBUG is enabled now!');
+            logger.info('INFO is enabled now!');
+            logger.warn('WARN is enabled now!');
+            logger.error('ERROR is enabled now!');
+            logger.fatal('FATAL is enabled now!');
+        }
+    });
 
-  const logger_export = {};
-  appenders.forEach(function(appender) {
-    let name = appender.category;
-    if (name) {
-      let logger = log4js.getLogger(name);
-      logger_export[name] = logger;
-      logger.setLevel(appender.level || 'ALL');
-      logger.trace('TRACE is enabled now!');
-      logger.debug('DEBUG is enabled now!');
-      logger.info('INFO is enabled now!');
-      logger.warn('WARN is enabled now!');
-      logger.error('ERROR is enabled now!');
-      logger.fatal('FATAL is enabled now!');
-    }
-  });
-
-  return logger_export;
+    return logger_export;
 }();
 
 //绑定到全局变量
